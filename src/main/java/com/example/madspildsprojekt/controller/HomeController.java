13569e56a40ff64799ec623fce86d5a3922f3e9a
christopherfuglsang <christeinfuglsang@gmail.com>
2025-04-04 11:18:58 +0200

    Gedehams

--- src/main/java/com/example/madspildsprojekt/controller/HomeController.java
+++ src/main/java/com/example/madspildsprojekt/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+// Merry er en fukin gedehams
 public class HomeController
 {
     @GetMapping("/")
